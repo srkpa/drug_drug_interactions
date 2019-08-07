d9e24c6c530042d11a65b760f16337650c04887f
srkpa <rogia@invivoai.com>
2019-08-07 15:15:28 -0400

    test-tensorboard repo add, launch_setting modified

---
+++ expts/test/config.py
@@ -0,0 +1,52 @@
+import click
+import json
+from sklearn.model_selection import ParameterGrid
+from ivbase.utils.constants.alphabet import SMILES_ALPHABET
+
+dataset_params = list(ParameterGrid(
+    dict(dataset_name=["twosides"],
+         transformer=["deepddi"],
+         split_mode=["random"],
+         test_size=[0.15],
+         valid_size=[0.10],
+         seed=[42]
+         )
+))
+
+fit_params = list(ParameterGrid(
+    dict(n_epochs=[2], batch_size=[256], with_early_stopping=[True])))
+
+network_params = list(ParameterGrid(dict(
+    network_name=['deepddi'],
+    input_dim=[100],
+    hidden_sizes=[[2048] * 9]
+)))
+
+model_params = list(ParameterGrid(dict(
+    network_params=network_params,
+    optimizer=['adam'],
+    lr=[1e-4],
+    loss=['bce'],
+    metrics_names=[None]
+)))
+
+
+@click.command()
+@click.option('-d', '--dataset', type=str, default='twosides', help='folder to scan')
+@click.option('-a', '--algo', type=str, default='bmnddi')
+@click.option('-o', '--outfile', type=str, default='configs.json')
+def generate_config_file(dataset, algo, outfile):
+    # todo: use argument of the function
+
+    expt_config = dict(
+        model_params=model_params,
+        dataset_params=dataset_params,
+        fit_params=fit_params
+    )
+
+    with open(outfile, 'w') as f:
+        json.dump(expt_config, f, indent=2)
+
+
+if __name__ == '__main__':
+    generate_config_file()
