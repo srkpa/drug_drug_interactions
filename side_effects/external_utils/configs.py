3a4239a22c44b167f138b8344d564013181e4ec5
srkpa <rogia@invivoai.com>
2019-08-26 14:56:50 -0400

    Launch graph drug expts

---
+++ side_effects/external_utils/configs.py
@@ -0,0 +1,23 @@
+import os
+import json
+from collections import defaultdict
+
+
+def merge_config_files(config_path, output_path):
+    configs_params = defaultdict(list)
+    i = 0
+    for file in os.listdir(config_path):
+        path = os.path.join(config_path, file)
+        if file.endswith(".json"):
+            param = json.load(open(path, "r"))
+            for param, val in param.items():
+                if val not in configs_params[param]:
+                    configs_params[param].append(val)
+            i += 1
+    print("Total json files= ", i)
+    json.dump(configs_params, open(os.path.join(output_path, "configs.json"), "w"), indent=2)
+
+
+if __name__ == '__main__':
+    merge_config_files("/home/rogia/Documents/analysis/results/graph_drug_prototype",
+                       output_path="/home/rogia/Documents/git/drug_drug_interactions/expts/graph_drug_expts")
